5f409fc164078ca4b6baa20bdb1d92aa22236052
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-10-14 03:07:20 -0700

    Change crate names

--- sdk/src/constants/index.ts
+++ sdk/src/constants/index.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const SPL_NOOP_ADDRESS = 'noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV';
-export const SPL_NOOP_PROGRAM_ID = new PublicKey(SPL_NOOP_ADDRESS);
+export const MPL_NOOP_ADDRESS = 'noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV';
+export const MPL_NOOP_PROGRAM_ID = new PublicKey(MPL_NOOP_ADDRESS);
 
 /**
  * DepthSizePair is a valid (`maxDepth`, `maxBufferSize`) tuple for an SPL ConcurrentMerkleTree
